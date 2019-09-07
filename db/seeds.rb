9cceaf988e26b199347003755b1ab5764b5fdba6
ryota misumi <3delta7@gmail.com>
2019-09-07 15:34:53 +0900

    add seed and modify migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Menu.find_or_create_by(
+    id: 1,
+    name: "いちごみるく",
+    shop_id: 1,
+    price: 600,
+    image_url: "",
+)
+Menu.find_or_create_by(
+    id: 2,
+    name: "バナナみるく",
+    shop_id: 1,
+    price: 650,
+    image_url: "",
+
+)
+Menu.find_or_create_by(
+    id: 3,
+    name: "チョコみるく",
+    shop_id: 1,
+    price: 700,
+    image_url: "",
+)
+if User.find_by(email: "aaa@gmail.com").nil?
+    User.create!(
+        name:"rika",
+        email:"aaa@gmail.com",
+        password:"123456"
+    )
+end
+if User.find_by(email: "bbb@gmail.com").nil?
+    User.create!(
+        name:"delta",
+        email:"bbb@gmail.com",
+        password:"234567"
+    )
+end
+if User.find_by(email: "ccc@gmail.com").nil?
+    User.create!(
+        name:"minako",
+        email:"ccc@gmail.com",
+        password:"345678"
+    )
+end
+if Shop.find_by(email: "ddd@gmail.com").nil?
+    User.create!(
+        name:"yagi",
+        email:"ddd@gmail.com",
+        password:"456789"
+    )
+end
